693c04e72382d9e245c56475974033f2e77aeb72
jaman-bala <ermekoffdastan@yandex.ru>
2024-03-18 17:35:06 +0600

    edit settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -5,9 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = ["http://monitoring.tsvs.kg"]
-# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
