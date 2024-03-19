d7cdf533eb8cc8a871c4a4c44181c27904140b66
jaman-bala <ermekoffdastan@yandex.ru>
2024-03-19 12:04:40 +0600

    edit admin.py

--- backend/apps/cameras/admin.py
+++ backend/apps/cameras/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Camera
 
+
 @admin.register(Camera)
 class CameraAdmin(admin.ModelAdmin):
     list_display = ("title", "id")
-    search_fields = "title"
+    search_fields = ("title",)
