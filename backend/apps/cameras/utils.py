e3119ac09c1de74613800ecb3a0bc2676e915adb
jaman-bala <ermekoffdastan@yandex.ru>
2024-08-29 13:30:06 +0600

    up:40 edit sync_to_async

--- backend/apps/cameras/utils.py
+++ backend/apps/cameras/utils.py
@@ -1,47 +1,49 @@
 import asyncio
 from asgiref.sync import sync_to_async
 import httpx
-
 from backend.apps.cameras.models import CameraModels
-from backend.apps.cameras.schemas import CameraOutput
-from backend.apps.cameras.schemas import CategorySchemas
-from backend.apps.cameras.schemas import RegionSchemas
+from backend.apps.cameras.schemas import CameraOutput, CategorySchemas, RegionSchemas
 
+# Настройка семафора для ограничения одновременных запросов
+SEMAPHORE_LIMIT = 10
+semaphore = asyncio.Semaphore(SEMAPHORE_LIMIT)
 
 async def check_ping(camera):
-    try:
-        async with httpx.AsyncClient(verify=False) as client:
-            url = camera.url
-            if url.startswith('http://'):
-                url_https = url.replace('http://', 'https://')
-            else:
-                url_https = url
-
-            # Пробуем соединиться с HTTPS
-            response = await client.get(url_https)
-            if response.status_code == 200:
-                return round(response.elapsed.total_seconds() * 100)
-
-    except httpx.RequestError:
-        # Пробуем соединиться с HTTP
+    async with semaphore:
         try:
             async with httpx.AsyncClient(verify=False) as client:
-                response = await client.get(url)
+                url = camera.url
+                if url.startswith('http://'):
+                    url_https = url.replace('http://', 'https://')
+                else:
+                    url_https = url
+
+                # Пробуем соединиться с HTTPS
+                response = await client.get(url_https)
                 if response.status_code == 200:
                     return round(response.elapsed.total_seconds() * 100)
+
         except httpx.RequestError:
-            pass
+            # Пробуем соединиться с HTTP
+            try:
+                async with httpx.AsyncClient(verify=False) as client:
+                    response = await client.get(url)
+                    if response.status_code == 200:
+                        return round(response.elapsed.total_seconds() * 100)
+            except httpx.RequestError:
+                pass
 
-    return 0
-
+        return 0
 
 async def get_cameras_with_ping():
+    # Использование кеша для хранения камер
     cameras = await sync_to_async(list)(CameraModels.objects.select_related('category').all())
     tasks = [check_ping(camera) for camera in cameras]
     ping_results = await asyncio.gather(*tasks)
 
     results = []
     for camera, ping_result in zip(cameras, ping_results):
+        # Использование кешированных данных
         category_data = CategorySchemas.from_orm(camera.category)
         region_data = RegionSchemas.from_orm(camera.region)
         camera_output = CameraOutput(
@@ -56,4 +58,3 @@ async def get_cameras_with_ping():
         results.append(camera_output)
 
     return results
-
